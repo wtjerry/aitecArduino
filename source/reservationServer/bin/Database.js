ecaf5096715599f94193d8cc4d602c36b5228d60
Dane Wicki (CryDeb) <wicki.dane@gmail.com>
2016-11-19 02:48:02 +0100

    working signing The signing function (on server site) is now working.

---
+++ source/reservationServer/bin/Database.js
@@ -0,0 +1,62 @@
+var Entities = require('html-entities').AllHtmlEntities;
+var mysql = require('mysql');
+var entities = new Entities();
+
+
+
+var exports = module.exports = {};
+
+exports.Database = function (){
+    this.pool = mysql.createPool({
+        connectionLimit : 100, //important
+        host     : '192.168.56.101',
+        user     : 'park_shit',
+        password : 'dachs',
+        database : 'park_shit',
+        port     : '3306',
+        debug    :  false
+
+    });
+    this.query = function(quer, callback){
+        this.pool.getConnection(function(error, connection){
+            if(error){
+                console.log("Error during the Database connection.");
+                callback("Error");
+            }
+            connection.query(quer,function(err,rows){
+                connection.release();
+                if(!err) {
+                    callback("", rows);
+                }
+            });
+            connection.on('error', function(err) {
+                callback(error);
+            });
+
+        });
+    };
+    this.query = function(quer, array, callback){
+        array.forEach(this.encapsulation);
+        this.pool.getConnection(function(error, connection){
+            if(error){
+                console.log("Error during databaseconnection.");
+                callback("Error");
+            }
+            connection.query(quer,array,function(err,rows){
+                connection.release();
+                if(!err) {
+                    callback("", rows);
+                }
+            });
+            connection.on('error', function(err) {
+                callback(error);
+            });
+
+        });
+
+    };
+    this.encapsulation = function(string){
+        string = entities.encode(string);
+    };
+
+}
